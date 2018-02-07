689d32981c8b8861ece5d13a0f11466c0d3d98c7
pjbest <zhanning.fan@pjbest.com>
2018-02-07 19:40:01 +0800

    init project

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: '',
+    output: {
+        path: path.resolve(__dirname, dist),
+        filename: 'bundle.js'
+    }
+}
