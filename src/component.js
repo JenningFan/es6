0dd90da8f1bb2722215edcf6f943a6f96579b760
fzning <zhanningFan@126.com>
2018-04-07 23:02:46 +0800

    add some comment

--- src/component.js
+++ src/component.js
@@ -5,12 +5,13 @@ export default class Component {
         this.props = props
     }
     setState(state) {
-        //记录旧的组件
+        //先用oldEl记录当前的(旧)组件形态
         const oldEl = this.el
         //更新state
         this.state = state
-        //改变this.el，指向新构建的DOM
+        //调用_renderDOM后会改变this.el，使之指向根据现有新的render函数来构建的新DOM
         this._renderDOM()
+        //第一次调用mount函数时，mount函数内就为组件注册的onStateChange函数，每次state有改变时都会调用此函数，将根据当前state render出来的新组件替换旧组件
         if (this.onStateChange) {
             this.onStateChange(oldEl, this.el)
         }
--- src/mount.js
+++ src/mount.js
@@ -1,7 +1,8 @@
 //类似于ReactDOM.render函数
 const mount = (component, wrapper) => {
+    //将根据render函数生成的fragment element(单个组件，也可能是组件树)添加到页面的wrapper中
     wrapper.appendChild(component._renderDOM())
-    //往传入的组件中注册onStateChange接口
+    //往传入的组件中注册onStateChange接口，组件每次被setState时都会调用该函数
     component.onStateChange = (oldEl, newEl) => {
         wrapper.insertBefore(newEl, oldEl)
         wrapper.removeChild(oldEl)
